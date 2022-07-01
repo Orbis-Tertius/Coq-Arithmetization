915821803e196a3c213170806fb096190d943422
Anthony Hart <soobtoob@gmail.com>
2022-06-30 18:24:20 -0700

    Add empty file to contain semicircuits

---
+++ theories/SemiCircuit.v
@@ -0,0 +1,13 @@
+From Hammer Require Import Tactics Reflect Hints.
+
+From mathcomp Require Import ssreflect ssrfun ssrbool eqtype ssrnat ssrint seq choice.
+From mathcomp Require Import fintype div intdiv tuple bigop prime finset fingroup.
+From mathcomp Require Import ssralg poly polydiv morphism action finalg zmodp.
+From mathcomp Require Import cyclic center pgroup abelian matrix mxpoly vector.
+From mathcomp Require Import falgebra fieldext separable galois.
+From mathcomp Require ssrnum ssrint.
+
+Section SemicircuitDef.
+
+
+End SemicircuitDef.
