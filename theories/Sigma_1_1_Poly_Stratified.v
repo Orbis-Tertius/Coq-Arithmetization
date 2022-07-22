982143e3505c4af9b4297f502325677b44b9ef14
Anthony Hart <soobtoob@gmail.com>
2022-07-21 22:53:01 -0700

    Factor out some utility functions.

--- theories/Sigma_1_1_Poly_Stratified.v
+++ theories/Sigma_1_1_Poly_Stratified.v
@@ -10,95 +10,6 @@ Require Import CoqArith.Utils.
 
 Require Import Program.
 
-(*tnth : forall (n : nat) (T : Type), n.-tuple T -> {m : m < n} -> T*)
-
-Definition lnth {T} (s : seq T) (i : {m | m < length s}) : T.
-  apply (tnth (in_tuple s)).
-  destruct i as [i lti].
-  by exists i.
-Defined.
-
-Theorem map_length {A B} (f : A -> B) (s : seq A) : length [seq f i | i <- s] = length s.
-Proof. move: s; elim; hauto q:on. Qed.
-
-Theorem Ordinal_Rect n n2 m (i : m < n) (e : n = n2) :
-  (eq_rect _ _ (Ordinal (n:=n) (m:=m) i) _ e) = Ordinal (eq_rect _ (fun x => m < x) i _ e).
-Proof. by destruct e. Qed.
-
-Theorem map_nth {A B} (f : A -> B) (s : seq A) (o : 'I_(length [seq f i | i <- s])) :
-  tnth (in_tuple [seq f i | i <- s]) o = 
-  f (tnth (in_tuple s) (eq_rect _ _ o _ (map_length _ _))).
-Proof.
-  move: s o; elim;[move=> [x xlt]; fcrush|].
-  simpl.
-  move=> a l IH o.
-  destruct o.
-  rewrite Ordinal_Rect.
-  rewrite (tnth_nth (f a)).
-  rewrite (tnth_nth a).
-  destruct m;[reflexivity|].
-  simpl.
-  assert (m < length [seq f i | i <- l]) as H;[hauto|].
-  transitivity (tnth (in_tuple [seq f i0 | i0 <- l]) (Ordinal H));[
-  by rewrite (tnth_nth (f a))|].
-  rewrite IH.
-  rewrite Ordinal_Rect.
-  by rewrite (tnth_nth a).
-Qed.
-
-Theorem map_lnth {A B} (f : A -> B) (s : seq A) (o : {m | m < length [seq f i | i <- s]}) :
-  lnth [seq f i | i <- s] o = 
-  f (lnth (in_tuple s) (eq_rect _ (fun x => {m : nat | m < x}) o _ (map_length _ _))).
-Proof.
-  unfold lnth.
-  rewrite map_nth.
-  do 2 f_equal.
-  destruct o; simpl.
-  apply ord_inj.
-  by transitivity (Ordinal 
-     (proj2_sig (eq_rect _ (fun x0 : nat => {m : nat | m < x0}) (exist _ x i) 
-     (length s) (map_length f s))));[destruct (map_length f s)|destruct (eq_rect _ _ _ _)].
-Qed.
-
-Theorem map_nth_2 {A B} (f : A -> B) (s : seq A) (o : 'I_(length s)) :
-  f (tnth (in_tuple s) o) 
-  = tnth (in_tuple [seq f i | i <- s]) (eq_rect _ _ o _ (esym (map_length f _))).
-Proof.
-  move: s o; elim;[move=> [x xlt]; fcrush|].
-  simpl.
-  move=> a l IH o.
-  destruct o.
-  rewrite Ordinal_Rect.
-  rewrite (tnth_nth (f a)).
-  rewrite (tnth_nth a).
-  destruct m;[reflexivity|].
-  simpl.
-  assert (m < length l) as H;[hauto|].
-  transitivity (f (tnth (in_tuple l) (Ordinal H)));[
-  by rewrite (tnth_nth a)|].
-  rewrite IH.
-  rewrite Ordinal_Rect.
-  by rewrite (tnth_nth (f a)).
-Qed.
-
-Lemma map_nth_3_lem 
-  {A T} {a b} (P Q : A -> Type) (tt : forall a, P a -> Q a -> T)
-  (e : a = b) (pa : P a) (qb : Q b) :
-  tt _ (eq_rect _ _ pa _ e) qb =
-  tt _ pa (eq_rect _ _ qb _ (esym e)).
-Proof. by destruct e. Qed.
-
-Theorem map_nth_3 {A B} (f : A -> B) (s : seq A) (o : 'I_(length s)) :
-  f (tnth (in_tuple s) o) 
-  = tnth (eq_rect _ (fun i => i.-tuple B) (in_tuple [seq f i | i <- s])
-         _ 
-         (map_length f _)) o.
-Proof.
-  rewrite map_nth_2.
-  by rewrite (map_nth_3_lem _ _ (fun x => @tnth x B) _ _ _).
-Qed.
-
-
 Section Sigma_1_1_Internal.
 
 Definition SOctx := seq nat.
@@ -591,113 +502,8 @@ End Sigma_1_1_Internal.
 
 Section Sigma_1_1_Denotation.
 
-Record SecondOrderFormulaModel : Type :=
-    mkSecondOrderFormulaModel {
-        R : ringType;
-        (*lt should be a strict, total order with a least element*)
-        lt : relation R;
-        so : StrictOrder lt;
-        lt_total : forall x y, (lt x y) + ((x==y) + (lt y x));
-        min : R;
-        least_elem : forall x, lt min x
-      }.
-
-Program Definition Subargs {n B} (f : forall i : {m : nat | m < n.+1}, B i) (i : {i : nat | i < n}) : B i := f i.
-
-Program Definition ArgExtend {n B} (b : B) (f : {m : nat | m < n} -> B) (i : {m : nat | m < n.+1}) :=
-  match i < n with
-  | true => f i
-  | false => b
-  end.
-
-Lemma OptionArgs_Lem {B} (i : {m : nat | m < 0}) : B i.
-Proof. fcrush. Qed.
-
-Definition idx_ord_iso {n} : {m : nat | m < n} -> 'I_n.
-  move=> [i lti].
-  by exists i.
-Defined.
-
-Definition ord_idx_iso {n} : 'I_n -> {m : nat | m < n}.
-  move=> [i lti].
-  by exists i.
-Defined.
-
-Theorem idx_ord_iso_canc {n} : forall i : {m : nat | m < n}, ord_idx_iso (idx_ord_iso i) = i.
-  by move=> [i lti].
-Qed.
-
-Theorem ord_idx_iso_canc {n} : forall i : 'I_n, idx_ord_iso (ord_idx_iso i) = i.
-  by move=> [i lti].
-Qed.
-
-Definition mktuple_idx {n} {T} : ({m : nat | m < n} -> T) -> n.-tuple T.
-  move=> f.
-  apply mktuple=> o.
-  apply f, ord_idx_iso, o.
-Defined.
-
-Definition tnth_idx {n} {T} : n.-tuple T -> ({m : nat | m < n} -> T).
-  move=> f i.
-  apply idx_ord_iso in i.
-  move: i.
-  by apply tnth.
-Defined.
-
-Theorem mktuple_idx_canc {n} {T} : forall f : {m : nat | m < n} -> T, tnth_idx (mktuple_idx f) = f.
-  move=> f.
-  apply functional_extensionality.
-  move=> [x ltx].
-  unfold tnth_idx, mktuple_idx.
-  by rewrite tnth_mktuple.
-Qed.
-
-Theorem mktuple_tnth : 
-  forall [n : nat] [T' : Type] (t : n.-tuple T'),
-  mktuple (tnth t) = t.
-Proof.
-  move=> n T t.
-  symmetry; apply (tuple_map_ord t).
-Qed.
-
-Theorem tnth_idx_canc {n} {T} : forall t : n.-tuple T, mktuple_idx (tnth_idx t) = t.
-  move=> t.
-  unfold tnth_idx, mktuple_idx.
-  transitivity [tuple tnth t o | o < n].
-  f_equal; apply functional_extensionality=> x.
-  by rewrite ord_idx_iso_canc.
-  apply mktuple_tnth.
-Qed.
-
-(*Propagated options up to function application *)
-Program Fixpoint OptionArgs {n} {B : nat -> Type} (f : forall i : {m : nat | m < n}, option (B i)) :
-  option (forall i : {m : nat | m < n}, B i) :=
-  match n with
-  | 0 => Some OptionArgs_Lem
-  | n.+1 => 
-    obind (fun fn : B n =>
-      obind (fun f : forall i : {m : nat | m < n}, B (` i) =>
-        Some (fun j : {m : nat | m < n.+1} =>
-          (if Nat.eqb j n as b0 return (Nat.eqb j n = b0 -> B j)
-            then fun jn0 : Nat.eqb (` j) n = true => 
-              fn
-            else fun jn1 : Nat.eqb j n = false => 
-              f j
-          ) (erefl (Nat.eqb j n)))) 
-        (OptionArgs (Subargs f))) 
-      (f n)
-  end.
-Next Obligation.
-  by apply (elimT (PeanoNat.Nat.eqb_spec _ _)) in jn0.
-Qed.
-Next Obligation.
-  apply EqNat.beq_nat_false, Compare_dec.not_eq in jn1.
-  assert ((j < n) \/ (n < j));[by destruct jn1;[left|right];apply (introT ltP)|clear jn1].
-  hauto use: contra_ltn_leq.
-Qed.
-
 (*How to write this nicer?*)
-Definition Rin_Denot_con (M : SecondOrderFormulaModel)
+Definition Rin_Denot_con (M : Sigma11Model)
   (v1 : seq (R M))
   (v2 : seq {y : (R M) & 
             {bs : seq (R M) & 
@@ -743,7 +549,7 @@ Definition Rin_Denot_con (M : SecondOrderFormulaModel)
 Defined.
 
 (*Interpreting a ring term with free variables as a function from ring elems. and functions. *)
-Program Fixpoint Ring_Denote (M : SecondOrderFormulaModel)
+Program Fixpoint Ring_Denote (M : Sigma11Model)
   (v1 : seq (R M))
   (v2 : seq {y : (R M) & 
             {bs : seq (R M) & 
@@ -770,7 +576,7 @@ Program Fixpoint Ring_Denote (M : SecondOrderFormulaModel)
   end.
 
 (*Interpreting a ring term with free variables as a function from ring elems. and functions. *)
-Program Fixpoint Poly_Denote (M : SecondOrderFormulaModel)
+Program Fixpoint Poly_Denote (M : Sigma11Model)
   (v1 : seq (R M))
   (r : @PolyTerm (length v1)) : R M :=
   match r with
@@ -782,7 +588,7 @@ Program Fixpoint Poly_Denote (M : SecondOrderFormulaModel)
   | PolyTimes r1 r2 => Poly_Denote M v1 r2 * Poly_Denote M v1 r1
   end.
 
-Fixpoint ZerothOrder_Denote (M : SecondOrderFormulaModel) 
+Fixpoint ZerothOrder_Denote (M : Sigma11Model) 
   (v1 : seq (R M))
   (v2 : seq {y : (R M) & 
             {bs : seq (R M) & 
@@ -807,7 +613,7 @@ Fixpoint ZerothOrder_Denote (M : SecondOrderFormulaModel)
     end
   end.
 
-Fixpoint FirstOrder_Denote (M : SecondOrderFormulaModel) 
+Fixpoint FirstOrder_Denote (M : Sigma11Model) 
   (v1 : seq (R M))
   (v2 : seq {y : (R M) & 
             {bs : seq (R M) & 
@@ -834,7 +640,7 @@ Fixpoint otraverse {T} (s : seq (option T)) : option (seq T) :=
   end.
 
 (*Interpreting a ring term with free variables as a function from ring elems. and functions. *)
-Program Fixpoint SecondOrder_Denote (M : SecondOrderFormulaModel) 
+Program Fixpoint SecondOrder_Denote (M : Sigma11Model) 
   (v1 : seq (R M))
   (v2 : seq {y : (R M) & 
             {bs : seq (R M) & 
--- theories/Utils.v
+++ theories/Utils.v
@@ -1,7 +1,12 @@
 From Hammer Require Import Tactics Reflect Hints.
 From Hammer Require Import Hammer.
 
-From mathcomp Require Import ssreflect ssrnat seq fintype tuple.
+From mathcomp Require Import ssreflect ssrfun ssrbool ssrnat ssralg seq fintype tuple eqtype.
+
+From Coq Require Import FunctionalExtensionality.
+From Coq Require Import Relation_Definitions RelationClasses.
+
+Require Import Program.
 
 Definition drop_index {T} (m : nat) (s : seq T) : seq T := 
   take m s ++ behead (drop m s).
@@ -125,3 +130,200 @@ Theorem tnth_tuple_index {T} {s : seq T} (x : T) {i} (lti : i < length s) :
   tnth (in_tuple (x :: s)) (Ordinal (n:=length (x :: s)) (m:=i.+1) lti) = 
   tnth (in_tuple s) (Ordinal lti).
 Proof. by do 2 rewrite (tnth_nth x). Qed.
+
+
+(*tnth : forall (n : nat) (T : Type), n.-tuple T -> {m : m < n} -> T*)
+
+Definition lnth {T} (s : seq T) (i : {m | m < length s}) : T.
+  apply (tnth (in_tuple s)).
+  destruct i as [i lti].
+  by exists i.
+Defined.
+
+Theorem map_length {A B} (f : A -> B) (s : seq A) : length [seq f i | i <- s] = length s.
+Proof. move: s; elim; hauto q:on. Qed.
+
+Theorem Ordinal_Rect n n2 m (i : m < n) (e : n = n2) :
+  (eq_rect _ _ (Ordinal (n:=n) (m:=m) i) _ e) = Ordinal (eq_rect _ (fun x => m < x) i _ e).
+Proof. by destruct e. Qed.
+
+Theorem map_nth {A B} (f : A -> B) (s : seq A) (o : 'I_(length [seq f i | i <- s])) :
+  tnth (in_tuple [seq f i | i <- s]) o = 
+  f (tnth (in_tuple s) (eq_rect _ _ o _ (map_length _ _))).
+Proof.
+  move: s o; elim;[move=> [x xlt]; fcrush|].
+  simpl.
+  move=> a l IH o.
+  destruct o.
+  rewrite Ordinal_Rect.
+  rewrite (tnth_nth (f a)).
+  rewrite (tnth_nth a).
+  destruct m;[reflexivity|].
+  simpl.
+  assert (m < length [seq f i | i <- l]) as H;[hauto|].
+  transitivity (tnth (in_tuple [seq f i0 | i0 <- l]) (Ordinal H));[
+  by rewrite (tnth_nth (f a))|].
+  rewrite IH.
+  rewrite Ordinal_Rect.
+  by rewrite (tnth_nth a).
+Qed.
+
+Theorem map_lnth {A B} (f : A -> B) (s : seq A) (o : {m | m < length [seq f i | i <- s]}) :
+  lnth [seq f i | i <- s] o = 
+  f (lnth (in_tuple s) (eq_rect _ (fun x => {m : nat | m < x}) o _ (map_length _ _))).
+Proof.
+  unfold lnth.
+  rewrite map_nth.
+  do 2 f_equal.
+  destruct o; simpl.
+  apply ord_inj.
+  by transitivity (Ordinal 
+     (proj2_sig (eq_rect _ (fun x0 : nat => {m : nat | m < x0}) (exist _ x i) 
+     (length s) (map_length f s))));[destruct (map_length f s)|destruct (eq_rect _ _ _ _)].
+Qed.
+
+Theorem map_nth_2 {A B} (f : A -> B) (s : seq A) (o : 'I_(length s)) :
+  f (tnth (in_tuple s) o) 
+  = tnth (in_tuple [seq f i | i <- s]) (eq_rect _ _ o _ (esym (map_length f _))).
+Proof.
+  move: s o; elim;[move=> [x xlt]; fcrush|].
+  simpl.
+  move=> a l IH o.
+  destruct o.
+  rewrite Ordinal_Rect.
+  rewrite (tnth_nth (f a)).
+  rewrite (tnth_nth a).
+  destruct m;[reflexivity|].
+  simpl.
+  assert (m < length l) as H;[hauto|].
+  transitivity (f (tnth (in_tuple l) (Ordinal H)));[
+  by rewrite (tnth_nth a)|].
+  rewrite IH.
+  rewrite Ordinal_Rect.
+  by rewrite (tnth_nth (f a)).
+Qed.
+
+Lemma map_nth_3_lem 
+  {A T} {a b} (P Q : A -> Type) (tt : forall a, P a -> Q a -> T)
+  (e : a = b) (pa : P a) (qb : Q b) :
+  tt _ (eq_rect _ _ pa _ e) qb =
+  tt _ pa (eq_rect _ _ qb _ (esym e)).
+Proof. by destruct e. Qed.
+
+Theorem map_nth_3 {A B} (f : A -> B) (s : seq A) (o : 'I_(length s)) :
+  f (tnth (in_tuple s) o) 
+  = tnth (eq_rect _ (fun i => i.-tuple B) (in_tuple [seq f i | i <- s])
+         _ 
+         (map_length f _)) o.
+Proof.
+  rewrite map_nth_2.
+  by rewrite (map_nth_3_lem _ _ (fun x => @tnth x B) _ _ _).
+Qed.
+
+
+Definition idx_ord_iso {n} : {m : nat | m < n} -> 'I_n.
+  move=> [i lti].
+  by exists i.
+Defined.
+
+Definition ord_idx_iso {n} : 'I_n -> {m : nat | m < n}.
+  move=> [i lti].
+  by exists i.
+Defined.
+
+Theorem idx_ord_iso_canc {n} : forall i : {m : nat | m < n}, ord_idx_iso (idx_ord_iso i) = i.
+  by move=> [i lti].
+Qed.
+
+Theorem ord_idx_iso_canc {n} : forall i : 'I_n, idx_ord_iso (ord_idx_iso i) = i.
+  by move=> [i lti].
+Qed.
+
+Definition mktuple_idx {n} {T} : ({m : nat | m < n} -> T) -> n.-tuple T.
+  move=> f.
+  apply mktuple=> o.
+  apply f, ord_idx_iso, o.
+Defined.
+
+Definition tnth_idx {n} {T} : n.-tuple T -> ({m : nat | m < n} -> T).
+  move=> f i.
+  apply idx_ord_iso in i.
+  move: i.
+  by apply tnth.
+Defined.
+
+Theorem mktuple_idx_canc {n} {T} : forall f : {m : nat | m < n} -> T, tnth_idx (mktuple_idx f) = f.
+  move=> f.
+  apply functional_extensionality.
+  move=> [x ltx].
+  unfold tnth_idx, mktuple_idx.
+  by rewrite tnth_mktuple.
+Qed.
+
+Theorem mktuple_tnth : 
+  forall [n : nat] [T' : Type] (t : n.-tuple T'),
+  mktuple (tnth t) = t.
+Proof.
+  move=> n T t.
+  symmetry; apply (tuple_map_ord t).
+Qed.
+
+Theorem tnth_idx_canc {n} {T} : forall t : n.-tuple T, mktuple_idx (tnth_idx t) = t.
+  move=> t.
+  unfold tnth_idx, mktuple_idx.
+  transitivity [tuple tnth t o | o < n].
+  f_equal; apply functional_extensionality=> x.
+  by rewrite ord_idx_iso_canc.
+  apply mktuple_tnth.
+Qed.
+
+
+Program Definition Subargs {n B} (f : forall i : {m : nat | m < n.+1}, B i) (i : {i : nat | i < n}) : B i := f i.
+
+Program Definition ArgExtend {n B} (b : B) (f : {m : nat | m < n} -> B) (i : {m : nat | m < n.+1}) :=
+  match i < n with
+  | true => f i
+  | false => b
+  end.
+
+Lemma OptionArgs_Lem {B} (i : {m : nat | m < 0}) : B i.
+Proof. fcrush. Qed.
+
+(*Propagated options up to function application *)
+Program Fixpoint OptionArgs {n} {B : nat -> Type} (f : forall i : {m : nat | m < n}, option (B i)) :
+  option (forall i : {m : nat | m < n}, B i) :=
+  match n with
+  | 0 => Some OptionArgs_Lem
+  | n.+1 => 
+    obind (fun fn : B n =>
+      obind (fun f : forall i : {m : nat | m < n}, B (` i) =>
+        Some (fun j : {m : nat | m < n.+1} =>
+          (if Nat.eqb j n as b0 return (Nat.eqb j n = b0 -> B j)
+            then fun jn0 : Nat.eqb (` j) n = true => 
+              fn
+            else fun jn1 : Nat.eqb j n = false => 
+              f j
+          ) (erefl (Nat.eqb j n)))) 
+        (OptionArgs (Subargs f))) 
+      (f n)
+  end.
+Next Obligation.
+  by apply (elimT (PeanoNat.Nat.eqb_spec _ _)) in jn0.
+Qed.
+Next Obligation.
+  apply EqNat.beq_nat_false, Compare_dec.not_eq in jn1.
+  assert ((j < n) \/ (n < j));[by destruct jn1;[left|right];apply (introT ltP)|clear jn1].
+  hauto use: contra_ltn_leq.
+Qed.
+
+
+Record Sigma11Model : Type :=
+    mkSigma11Model {
+        R : ringType;
+        (*lt should be a strict, total order with a least element*)
+        lt : relation R;
+        so : StrictOrder lt;
+        lt_total : forall x y, (lt x y) + ((x==y) + (lt y x));
+        min : R;
+        least_elem : forall x, lt min x
+      }.
